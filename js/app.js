592e29259e0a27cd063ffbb4f3105d9297bc6bb5
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2023-08-15 21:29:27 +0300

    start

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener("DOMContentLoaded", (function() {
+        const navbarToggler = document.querySelector(".navbar-toggler");
+        document.querySelector(".navbar-nav");
+        navbarToggler.addEventListener("click", (function() {
+            if (!navbarToggler.classList.contains("collapsed")) navbarToggler.classList.add("collapsed"); else navbarToggler.classList.remove("collapsed");
+        }));
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
