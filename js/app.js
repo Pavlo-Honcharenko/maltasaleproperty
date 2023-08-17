e10a904e5ed1fdbe137b4ed240e1f71c84f06824
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2023-08-17 08:35:37 +0300

    835

--- js/app.js
+++ js/app.js
@@ -29,6 +29,89 @@
             if (!navbarToggler.classList.contains("collapsed")) navbarToggler.classList.add("collapsed"); else navbarToggler.classList.remove("collapsed");
         }));
     }));
+    function handleLiClick(e) {
+        if (e.target.type === "checkbox") return;
+        var checkbox = e.currentTarget.querySelector('input[type="checkbox"]');
+        if (checkbox) {
+            checkbox.checked = !checkbox.checked;
+            if (checkbox.checked) e.currentTarget.classList.add("_checked"); else e.currentTarget.classList.remove("_checked");
+        }
+        e.stopPropagation();
+    }
+    document.querySelectorAll(".filters li").forEach((function(li) {
+        li.addEventListener("click", handleLiClick);
+    }));
+    document.getElementById("selectAllPropertyType").addEventListener("click", (function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        document.querySelectorAll(".property-checkbox").forEach((function(checkbox) {
+            checkbox.checked = true;
+            checkbox.classList.add("_checked");
+        }));
+    }));
+    document.getElementById("clearAllPropertyType").addEventListener("click", (function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        document.querySelectorAll(".property-checkbox").forEach((function(checkbox) {
+            checkbox.checked = false;
+            checkbox.classList.remove("_checked");
+        }));
+    }));
+    function handleSelectClearAll(selectId, clearId, checkboxClass) {
+        document.getElementById(selectId).addEventListener("click", (function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            document.querySelectorAll(checkboxClass).forEach((function(checkbox) {
+                checkbox.checked = true;
+            }));
+        }));
+        document.getElementById(clearId).addEventListener("click", (function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            document.querySelectorAll(checkboxClass).forEach((function(checkbox) {
+                checkbox.checked = false;
+            }));
+        }));
+    }
+    handleSelectClearAll("selectAllSDA", "clearAllSDA", ".sda-checkbox");
+    handleSelectClearAll("selectAllMalta", "clearAllMalta", ".malta-checkbox");
+    handleSelectClearAll("selectAllGozo", "clearAllGozo", ".gozo-checkbox");
+    var priceSlider = document.getElementById("priceSlider");
+    noUiSlider.create(priceSlider, {
+        start: [ 1e3, 5e3 ],
+        connect: true,
+        step: 100,
+        range: {
+            min: 0,
+            max: 1e4
+        }
+    });
+    var fromPrice = document.getElementById("fromPrice");
+    var toPrice = document.getElementById("toPrice");
+    priceSlider.noUiSlider.on("update", (function(values, handle) {
+        var value = values[handle];
+        if (handle) toPrice.value = value; else fromPrice.value = value;
+    }));
+    fromPrice.addEventListener("change", (function() {
+        priceSlider.noUiSlider.set([ this.value, null ]);
+    }));
+    toPrice.addEventListener("change", (function() {
+        priceSlider.noUiSlider.set([ null, this.value ]);
+    }));
+    document.getElementById("selectAllFeatures").addEventListener("click", (function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        document.querySelectorAll(".features-checkbox").forEach((function(checkbox) {
+            checkbox.checked = true;
+        }));
+    }));
+    document.getElementById("clearAllFeatures").addEventListener("click", (function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        document.querySelectorAll(".features-checkbox").forEach((function(checkbox) {
+            checkbox.checked = false;
+        }));
+    }));
     window["FLS"] = true;
     isWebp();
 })();
